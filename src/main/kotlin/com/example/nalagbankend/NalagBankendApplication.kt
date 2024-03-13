d96f772ec25646b3eb6c6503abe2cadeb0dc9c15
juyeongno1 <juyeongno1@naver.com>
2024-03-13 10:26:07 +0900

    set :: project setting

---
+++ src/main/kotlin/com/example/nalagbankend/NalagBankendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nalagbankend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NalagBankendApplication
+
+fun main(args: Array<String>) {
+    runApplication<NalagBankendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/nalagbankend/NalagBankendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nalagbankend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NalagBankendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
