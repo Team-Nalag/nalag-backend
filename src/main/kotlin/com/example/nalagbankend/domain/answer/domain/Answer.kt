9ed100aab480598d628cea5eb0e9d2536ba6b8d4
jyk1029 <jyk102999@gmail.com>
2024-05-31 17:32:00 +0900

    modify :: column

--- src/main/kotlin/com/example/nalagbankend/domain/answer/domain/Answer.kt
+++ src/main/kotlin/com/example/nalagbankend/domain/answer/domain/Answer.kt
@@ -1,15 +1,32 @@
 package com.example.nalagbankend.domain.answer.domain
 
+import com.example.nalagbankend.domain.quiz.domain.Choice
 import com.example.nalagbankend.domain.quiz.domain.Quiz
 import com.example.nalagbankend.domain.user.domain.User
 import com.example.nalagbankend.global.entity.BaseUUIDEntity
-import javax.persistence.Column
 import javax.persistence.Entity
+import javax.persistence.FetchType.LAZY
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
 
 @Entity(name = "tbl_answer")
 class Answer(
-    @Column(columnDefinition = "CHAR(50)", nullable = false)
-    val choice: String,
+    choice: Choice,
     user: User,
     quiz: Quiz
-): BaseUUIDEntity()
+) : BaseUUIDEntity() {
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
+    var user = user
+        protected set
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
+    var quiz = quiz
+        protected set
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
+    var choice = choice
+        protected set
+}
