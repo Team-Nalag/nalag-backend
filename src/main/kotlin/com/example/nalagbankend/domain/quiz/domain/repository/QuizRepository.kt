def2a60218419b885534e578e8c5e7e82f7342d0
jyk1029 <jyk102999@gmail.com>
2024-05-31 13:43:03 +0900

    chore :: code format

--- src/main/kotlin/com/example/nalagbankend/domain/quiz/domain/repository/QuizRepository.kt
+++ src/main/kotlin/com/example/nalagbankend/domain/quiz/domain/repository/QuizRepository.kt
@@ -4,5 +4,4 @@ import com.example.nalagbankend.domain.quiz.domain.Quiz
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.UUID
 
-interface QuizRepository: JpaRepository<Quiz, UUID> {
-}
+interface QuizRepository: JpaRepository<Quiz, UUID>
